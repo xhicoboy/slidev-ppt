1df6497646389dda89bb97eb274cee88fac38d8d
Hico <15768236093@163.com>
2025-12-22 15:24:14 +0800

    feat: 添加全局logo;

---
+++ global-top.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="fixed top-2 left-6 z-[100]" style="pointer-events: none;">
+    <img src="/logo.png" alt="Logo" class="w-20 h-20 object-contain" />
+  </div>
+</template>
+
